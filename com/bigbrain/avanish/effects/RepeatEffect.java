26f0bf3329f5691accd0ba16630ca37f287d8a41
Avanish <avanishde@gmail.com>
2025-02-27 14:40:21 +0100

    move addEffect to Effect parent class and made it static so that it can be accessed by RepeatEffect

--- com/bigbrain/avanish/effects/RepeatEffect.java
+++ com/bigbrain/avanish/effects/RepeatEffect.java
@@ -2,20 +2,25 @@ package com.bigbrain.avanish.effects;
 
 import com.bigbrain.avanish.effects.util.EffectType;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import static com.bigbrain.avanish.util.Commands.END;
 
 public class RepeatEffect extends Effect {
-
+    private final List<Effect> repeatedEffects = new ArrayList<>();
 
     public RepeatEffect(String[] currentLine, List<String> configFile) {
         super(EffectType.REPEAT, 0);
         while (!currentLine[0].equals(END)) {
-//make add effect static method - maybe create a factory class to create effects and add them to a given action
+            Effect.addEffect(repeatedEffects, currentLine, configFile);
             currentLine = configFile.remove(0).split(" ");
         }
 
+    }
+
+    public List<Effect> getRepeatedEffects() {
+        return repeatedEffects;
     }
 }
